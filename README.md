4b0daa2b9b81e45cad30571dc566ef3b24809e9a
MorenoJoshua <joshua200128@gmail.com>
2021-11-29 20:01:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-7xb6pb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-7xb6pb)
---
+++ index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Avatar from 'avataaars';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './style.scss';
+const colors = {
+  main: '#F0E9D2',
+  secondary: '#E6DDC4',
+  highlight: '#678983',
+  dark: '#181D31',
+};
+
+const name = 'Joshua Moreno';
+const position = 'Software Engineer';
+const niche = 'Mobile Web';
+const phone = '+52 664 355 7882';
+const email = 'hello@morenojoshua.com';
+
+const App = () => {
+  return (
+    <div
+      className="holder"
+      style={{
+        backgroundColor: colors.main,
+      }}
+    >
+      <div className="fcc fs">
+        <div className="j-card">
+          <Avatar
+            className="avatar"
+            avatarStyle="Circle"
+            topType="ShortHairShortCurly"
+            accessoriesType="Blank"
+            hairColor="Black"
+            facialHairType="BeardLight"
+            facialHairColor="Black"
+            clotheType="Hoodie"
+            clotheColor="Red"
+            eyeType="Default"
+            eyebrowType="UpDown"
+            mouthType="Smile"
+            skinColor="Light"
+          />
+          <div className="text">
+            <div className="name">{name}</div>
+            <div className="other">{position}</div>
+            <div className="other">{phone}</div>
+            <div className="other">{email}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+render(<App />, document.getElementById('root'));
